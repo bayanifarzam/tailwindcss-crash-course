4ec62637c92fbb3f8f3f96de223c640c31dd1afd
Raza Bayani <bayanifarzam@gmail.com>
2022-01-30 16:32:08 +0500

    Tailwind Customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}","index.html","./public/item.html"],
-  
+  content: ['./src/**/*.{html,js}','*.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'rubik':'Rubik',
+        'super':'super'
+      },
+      screens:{
+        'xs':'340px'
+      },
+      colors:{
+        'light-gold':"#f0f246cf",
+        'dark-gold':"#f0f246"
+      }
+      
+    },
   },
   plugins: [],
 }
